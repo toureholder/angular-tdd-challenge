8aae07979281fb3fb1f3b8df40773f59e7feffe8
Touré Holder <toureh@hotmail.com>
2020-06-11 16:52:45 -0300

    simplify movie serice

--- src/app/core/services/movie.service.ts
+++ src/app/core/services/movie.service.ts
@@ -1,42 +1,25 @@
-import { Injectable } from '@angular/core';
-import { BaseService } from './base.service';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Movie } from 'src/app/shared/models/movie';
-import { movieDb } from 'src/app/moviedb-config';
-import { map, catchError, retry } from 'rxjs/operators';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { movieDb } from "src/app/moviedb-config";
+import { Movie } from "src/app/shared/models/movie";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
-export class MovieService extends BaseService {
+export class MovieService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) {
-    super();
+  getUpcomingMovies(): Observable<Movie[]> {
+    return this.http
+      .get<any>(
+        `${movieDb.url}upcoming?api_key=${movieDb.api_key}&language=pt-BR&page=1`
+      )
+      .pipe(map(this.jsonDataToMovies));
   }
 
-  getUpcomingMovies(): Observable<any> {
-    return this.http.get<any>(`${movieDb.url}upcoming?api_key=${movieDb.api_key}&language=pt-BR&page=1`)
-      .pipe(
-        retry(1),
-        catchError(this.handleError), map(this.jsonDataToMovies));
+  private jsonDataToMovies(jsonData: any): Movie[] {
+    return jsonData && jsonData.results ? jsonData.results : [];
   }
-
-  getMovie(id: string): Observable<Movie> {
-    return this.http.get<Movie>(`${movieDb.url}${id}?api_key=${movieDb.api_key}&language=pt-BR`)
-      .pipe(
-        retry(1),
-        catchError(this.handleError));
-  }
-
-  jsonDataToMovies(jsonData: any): Movie[] {
-    const movies: Movie[] = [];
-    if (jsonData && jsonData.results) {
-      jsonData.results.forEach((movie: Movie) => {
-        movies.push(movie);
-      });
-    }
-    return movies;
-  }
-
 }
