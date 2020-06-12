5261f84746f90493764c12c10bd660ff12aec72e
Touré Holder <toureh@hotmail.com>
2020-06-12 17:31:02 -0300

    Write more card-movie component tests

--- src/app/modules/movies/card-movie/card-movie.component.spec.ts
+++ src/app/modules/movies/card-movie/card-movie.component.spec.ts
@@ -1,25 +1,53 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { CardMovieComponent } from "./card-movie.component";
+import { movieDb } from "src/app/moviedb-config";
 
-import { CardMovieComponent } from './card-movie.component';
-
-describe('CardMovieComponent', () => {
+describe("CardMovieComponent", () => {
   let component: CardMovieComponent;
   let fixture: ComponentFixture<CardMovieComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CardMovieComponent ]
-    })
-    .compileComponents();
+      declarations: [CardMovieComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CardMovieComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should generate correct image url", () => {
+    component.movie = {
+      id: "514847",
+      poster_path: "/mDt3GkephI5yrRsEgLfdo3MYxyj.jpg",
+      title: "A Caçada",
+    };
+
+    component.ngOnInit();
+
+    expect(component.urlImage).toBe(
+      movieDb.url_image + component.movie.poster_path
+    );
+  });
+
+  it("should render img with url", () => {
+    component.movie = {
+      id: "514847",
+      poster_path: "/mDt3GkephI5yrRsEgLfdo3MYxyj.jpg",
+      title: "A Caçada",
+    };
+
+    component.ngOnInit();
+
+    fixture.detectChanges();
+
+    const template: HTMLElement = fixture.nativeElement;
+
+    expect(template.querySelector("img").src).toBe(component.urlImage);
   });
 });
