2a63daf82e7b2d3badfe77bbfafd03e132b5711a
Touré Holder <toureholder@gmail.com>
2020-06-12 07:23:37 -0300

    small impl improvement

--- src/app/core/services/movie.service.ts
+++ src/app/core/services/movie.service.ts
@@ -13,13 +13,13 @@ export class MovieService {
 
   getUpcomingMovies(): Observable<Movie[]> {
     return this.http
-      .get<any>(
+      .get(
         `${movieDb.url}upcoming?api_key=${movieDb.api_key}&language=pt-BR&page=1`
       )
       .pipe(map(this.jsonDataToMovies));
   }
 
-  private jsonDataToMovies(jsonData: any): Movie[] {
+  private jsonDataToMovies(jsonData): Movie[] {
     return jsonData && jsonData.results ? jsonData.results : [];
   }
 }
--- src/app/modules/movies/list-movies/list-movies.component.ts
+++ src/app/modules/movies/list-movies/list-movies.component.ts
@@ -1,27 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { Movie } from 'src/app/shared/models/movie';
-import { MovieService } from 'src/app/core/services/movie.service';
+import { Component, OnInit } from "@angular/core";
+import { Movie } from "src/app/shared/models/movie";
+import { MovieService } from "src/app/core/services/movie.service";
 
 @Component({
-  selector: 'app-list-movies',
-  templateUrl: './list-movies.component.html',
-  styleUrls: ['./list-movies.component.scss']
+  selector: "app-list-movies",
+  templateUrl: "./list-movies.component.html",
+  styleUrls: ["./list-movies.component.scss"],
 })
 export class ListMoviesComponent implements OnInit {
-
   movies: Movie[];
   loading: boolean;
 
-  constructor(
-    private movieService: MovieService
-  ) { }
+  constructor(private movieService: MovieService) {}
 
   ngOnInit(): void {
     this.loading = true;
-    this.movieService.getUpcomingMovies().subscribe(movieReturn => {
-      this.movies = movieReturn;
+    this.movieService.getUpcomingMovies().subscribe((data) => {
+      this.movies = data;
       this.loading = false;
     });
   }
-
 }
