94c2874e1d0176e07d92f7dc096f2adb2c5b907f
Touré Holder <toureh@hotmail.com>
2020-06-12 18:30:15 -0300

    add fakelist to movie model

--- src/app/shared/models/movie.ts
+++ src/app/shared/models/movie.ts
@@ -2,4 +2,12 @@ export class Movie {
   id: string;
   poster_path: string;
   title: string;
+
+  static FAKE_LIST: Movie[] = [
+    {
+      id: "514847",
+      poster_path: "/mDt3GkephI5yrRsEgLfdo3MYxyj.jpg",
+      title: "A Caçada",
+    },
+  ];
 }
