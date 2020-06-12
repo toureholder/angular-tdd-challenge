86c547ceadbc56249df5eb0a58f5426e34921180
Touré Holder <toureh@hotmail.com>
2020-06-12 17:29:59 -0300

    improve card-movie implementation

--- src/app/modules/movies/card-movie/card-movie.component.ts
+++ src/app/modules/movies/card-movie/card-movie.component.ts
@@ -1,22 +1,18 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Movie } from 'src/app/shared/models/movie';
-import { movieDb } from 'src/app/moviedb-config';
+import { Component, OnInit, Input } from "@angular/core";
+import { Movie } from "src/app/shared/models/movie";
+import { movieDb } from "src/app/moviedb-config";
 
 @Component({
-  selector: 'app-card-movie',
-  templateUrl: './card-movie.component.html',
-  styleUrls: ['./card-movie.component.scss']
+  selector: "app-card-movie",
+  templateUrl: "./card-movie.component.html",
+  styleUrls: ["./card-movie.component.scss"],
 })
 export class CardMovieComponent implements OnInit {
-
-  @Input() movie!: Movie;
+  @Input() movie: Movie;
 
   urlImage: string;
 
-  constructor() { }
-
   ngOnInit(): void {
-    this.urlImage = movieDb.url_image;
+    this.urlImage = movieDb.url_image + this.movie.poster_path;
   }
-
 }
